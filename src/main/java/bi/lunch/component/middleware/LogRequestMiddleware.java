2a6c753c48a298fefbc7edc9b932d9e977f1b16a
weirdobeardo48 <15998072+weirdobeardo48@users.noreply.github.com>
2020-08-06 15:58:43 +0700

    - Add a little comment

--- src/main/java/bi/lunch/component/middleware/LogRequestMiddleware.java
+++ src/main/java/bi/lunch/component/middleware/LogRequestMiddleware.java
@@ -15,6 +15,7 @@ import org.apache.log4j.*;
 public class LogRequestMiddleware extends OncePerRequestFilter {
 	private static final Logger log = Logger.getLogger(LogRequestMiddleware.class.getName());
 
+	// Temporary bypassing for testing
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
